b2ecd06945d717ee9d61cd09fcd6d3d3ebd07720
SergeyGuchok <gck.sergey@gmail.com>
2024-04-19 09:54:34 +0000

    changes

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,11 @@ import { REQUIRED_WORDS } from './constants/index.js';
 const TweetControllerInstance = new TweetController();
 const ScalpControllerInstance = new ScalpController();
 
-const cronTweet = new CronJob('*/85 * * * *', async () => {
+const cronTweet = new CronJob('*/47 * * * *', async () => {
   await TweetControllerInstance.tweetGeneratedByModalTweet();
 });
 
-const scalpReplyAndLike = new CronJob('*/60 * * * *', async () => {
+const scalpReplyAndLike = new CronJob('*/30 */2 * * *', async () => {
   const string = `(${REQUIRED_WORDS.map((e) => `"${e}"`).join(' OR ')}) is:verified -is:reply -is:retweet`;
 
   const tweets = await ScalpControllerInstance.scalpLastTenTweetsByString(string);
